ea1a1afb62cb66557c95fd84f9f51e0b90715860
T-126 <rathnayaka3548@gmail.com>
2024-04-23 20:56:17 +0530

    feat: moved data to config

---
+++ src/config/Index.tsx
@@ -0,0 +1,11 @@
+export const MYDATA = [
+  {
+    name: "Thilina Rathnayaka",
+    title: "Web Developer",
+    whatsappDisplay: "+94 71 891 3548",
+    whatsapp: "718913548",
+    instagram: "its_thili.rathnayaka",
+    email: "rathnayaka3548@gmail.com",
+    website: "https://edward-hyde.vercel.app/",
+  },
+];
